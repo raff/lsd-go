03f174b07b50b970dbeddcc89b0cc17ff4fecb2e
Raffaele Sena <raff367@gmail.com>
2019-05-23 00:16:34 -0700

    Fixed conversion to grayscale and added options to generate CSV or new image

--- cmd/lsd/main.go
+++ cmd/lsd/main.go
@@ -7,8 +7,10 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+	"math"
 	"os"
 
+	"github.com/fogleman/gg"
 	"github.com/raff/lsd-go"
 )
 
@@ -20,8 +22,8 @@ func makeGray(ima image.Image) ([]float64, int, int) {
 	gray := make([]float64, width*height)
 	gp := 0
 
-	for x := bounds.Min.X; x < bounds.Max.X; x++ {
-		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			//fmt.Println("x", x, "y", y, "gp", gp, "size", len(gray))
 			c := ima.At(x, y)
 			r, g, b, _ := c.RGBA()
@@ -46,6 +48,9 @@ func main() {
 	ueps := flag.Float64("union-eps", -1.0, "Union detection threshold")
 	ulength := flag.Float64("union-length", 5.0, "Minimum length of segment to union.")
 	udist := flag.Float64("union-distance", 5.0, "Maximum distance between two line which we would union.")
+	outf := flag.String("format", "verbose", "Output format (verbose, csv, image)")
+	color := flag.String("color", "#C0C0C0", "RGB color to use to highlight lines")
+	length := flag.Float64("length", 0, "Minimum line length")
 
 	flag.Parse()
 
@@ -91,10 +96,68 @@ func main() {
 			*ulength,
 			*udist)
 
-		for i := 0; i < n; i++ {
-			l := i * 7
-			fmt.Printf("%4d: x1:%4.1f y1:%4.1f x2:%4.1f y2:%4.1f w:%3.1f p:%3.2f nfa:%5.1f\n",
-				i, segs[l+0], segs[l+1], segs[l+2], segs[l+3], segs[l+4], segs[l+5], segs[l+6])
+		l := 0
+
+		switch *outf {
+		case "image":
+			gc := gg.NewContextForImage(ima)
+			gc.SetLineCap(gg.LineCapSquare)
+			gc.SetHexColor(*color)
+
+			/*
+			   gc.DrawRectangle(
+			       float64(ima.Bounds().Min.X+1),
+			       float64(ima.Bounds().Min.Y+1),
+			       float64(ima.Bounds().Max.X-1),
+			       float64(ima.Bounds().Max.Y-1))
+			   gc.SetLineWidth(4)
+			   gc.Stroke()
+			*/
+
+			// x1,y1,x2,y2,width,p,nfa
+			for i := 0; i < n; i++ {
+				x1, y1, x2, y2, width := segs[l+0], segs[l+1], segs[l+2], segs[l+3], segs[l+4]
+
+				// fmt.Println(i, "x1", x1, "x2", x2, "y1", y1, "y2", y2)
+
+				w, h := math.Abs(x2-x1), math.Abs(y2-y1)
+
+				if w != 0.0 && w < *length {
+					goto continue_line
+				}
+
+				if h != 0.0 && h < *length {
+					goto continue_line
+				}
+
+				gc.DrawLine(x1, y1, x2, y2)
+				gc.SetLineWidth(width)
+				gc.Stroke()
+			continue_line:
+				l += 7
+			}
+
+			gc.SavePNG("lines.png")
+
+		case "csv":
+			fmt.Println("x1,y1,x2,y2,width,p,nfa")
+			for i := 0; i < n; i++ {
+				fmt.Printf("%f,%f,%f,%f,%f,%f,%f\n",
+					segs[l+0], segs[l+1], segs[l+2], segs[l+3], segs[l+4], segs[l+5], segs[l+6])
+
+				l += 7
+			}
+
+		default:
+			for i := 0; i < n; i++ {
+				x1, y1, x2, y2 := segs[l+0], segs[l+1], segs[l+2], segs[l+3]
+
+				fmt.Printf("%4.0d %2d %2d: x1:%6.1f y1:%6.1f x2:%6.1f y2:%6.1f w:%3.1f p:%3.2f nfa:% 5.1f\n",
+					i+1, int(math.Abs(x2-x1)), int(math.Abs(y2-y1)),
+					segs[l+0], segs[l+1], segs[l+2], segs[l+3], segs[l+4], segs[l+5], segs[l+6])
+
+				l += 7
+			}
 		}
 	}
 }
